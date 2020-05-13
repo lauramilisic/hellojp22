bacbd3f08744e5b6857642df1672425528957bc8
Laura Milišić <laura.milisic@outlook.com>
2020-05-13 20:41:48 +0200

    početak

--- galerija.sql
+++ galerija.sql
@@ -1,1 +1,19 @@
-#
+## c:\xampp\mysql\bin\mysql -uedunova -pedunova < c:\jp22\hello\galerija.sql
+
+drop database if exists galerija;
+create database galerija;
+use galerija;
+
+reate table autor(
+    sifra int not null primary key auto_increment,
+    ime varchar(50) not null,
+    prezime varchar(50) not null,
+    oib char(11),
+    email varchar(100),
+    iban varchar(50)
+);
+create table dijelo(
+    sifra int not null primary key auto_increment,
+    naziv varchar(50) not null,
+    
+);
